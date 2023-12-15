005b13b8e2bf10beeed575bd6df3e6e11b01de83
EhsanEsmaily747 <ehsanesmaily747@gmail.com>
2023-12-15 16:35:26 +0430

    category delete error fixed

--- server/api/categories/category.delete.js
+++ server/api/categories/category.delete.js
@@ -1,13 +1,21 @@
 
 import Category from "~/server/models/category";
+import Post from '~/server/models/post'
 
 export default defineEventHandler(async(event)=>{
     const query=getQuery(event)
-
+    const filter = {}
     const exist=await Category.findById(query.id)
 
     if (exist) {
         exist.deleteOne()
+        filter.category=query.id
+        const posts = await Post.find(filter)
+
+        for (let index = 0; index < posts.length; index++) {
+            posts[index].category='65576fedfbdf37c88b835229'
+            posts[index].save()  
+        }
         return `Category ${exist} Deleted`
     }
     return `Category Doesn't Exit`
